b1c0dee16a525e2434435dc822c635a817d21615
tuomax7 <tuomas.nummela.per@gmail.com>
2023-03-19 14:25:22 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>LuomuLuola</h1>
+      <div>Video tähän!</div>
+      <div>Nopea esittely tähän!</div>
+      <footer>Kreditit musiikille ja logopohjille</footer>
     </div>
   );
-}
+};
 
 export default App;
