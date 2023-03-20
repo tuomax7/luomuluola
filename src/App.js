efe151b0fa786a8ed0ff11af2077cea54cddf733
tuomax7 <tuomas.nummela.per@gmail.com>
2023-03-21 00:45:22 +0200

    depoloy.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,65 @@
-import React from 'react'
-import { Typography, Box, createTheme, ThemeProvider, CssBaseline } from '@mui/material'
-import VideoPlayer from './components/VideoPlayer'
-import Introduction from './components/Introduction'
-import Footer from './components/Footer'
+import React from "react";
+import {
+  Typography,
+  Box,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@mui/material";
+import VideoPlayer from "./components/VideoPlayer";
+import Introduction from "./components/Introduction";
+import Footer from "./components/Footer";
 
 const customTheme = createTheme({
   root: {
-    fontFamily: 'sans-serif'
+    fontFamily: "sans-serif",
   },
   palette: {
     background: {
-      default: '#CDA169'
+      default: "#CDA169",
     },
     text: {
-      primary: '#ffffff'
+      primary: "#ffffff",
     },
     primary: {
-      main: '#CDA169'
+      main: "#CDA169",
     },
     secondary: {
-      main: '#926E50'
-    }
-  }
-})
+      main: "#926E50",
+    },
+  },
+});
 
 const App = () => {
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
-      <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' minHeight='100vh'>
-        <Box display='flex' flexDirection='row'>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Box display="flex" flexDirection="row">
           <Box
-            component='img'
+            component="img"
             sx={{
               height: 100,
-              width: 100
+              width: 100,
             }}
-            src='luomuluolalogo.png'
+            src="https://users.aalto.fi/~nummelt3/luomuluolalogo.png"
           ></Box>
-          <Typography variant='h1'>LuomuLuola</Typography>
+          <Typography variant="h1">LuomuLuola</Typography>
         </Box>
 
-        <Typography variant='h2'>Hyppää luomuostoksille!</Typography>
+        <Typography variant="h2">Hyppää luomuostoksille!</Typography>
         <VideoPlayer />
         <Introduction />
         <Footer />
       </Box>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import { IconButton } from '@mui/material'
-import PlayArrow from '@mui/icons-material/PlayArrow'
-import ReactPlayer from 'react-player'
+import React from "react";
+import { IconButton } from "@mui/material";
+import PlayArrow from "@mui/icons-material/PlayArrow";
+import ReactPlayer from "react-player";
 
 const VideoPlayer = () => {
   return (
     <ReactPlayer
-      url='https://users.aalto.fi/~nummelt3/promovideo.mp4'
+      url="https://users.aalto.fi/~nummelt3/promovideo.mp4"
       controls
       playIcon={
-        <IconButton aria-label='play-video'>
-          <PlayArrow sx={{ transform: 'scale(8)' }} color='secondary' />
+        <IconButton aria-label="play-video">
+          <PlayArrow sx={{ transform: "scale(8)" }} color="secondary" />
         </IconButton>
       }
-      light='promovideotease.png'
+      light="https://users.aalto.fi/~nummelt3/promovideotease.png"
       playing
       width={800}
       height={450}
     />
-  )
-}
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
