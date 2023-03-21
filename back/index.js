45df6c6ef6a1ab735cb30d852e3fc5e6841f0599
tuomax7 <tuomas.nummela.per@gmail.com>
2023-03-21 17:16:18 +0200

    Production build.

--- back/index.js
+++ back/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 app.use(cors());
 
+app.use(express.static("build"));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
